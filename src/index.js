459434a8d86cdb34f7f74aa11240659bbc3ae72e
Alexander Sobolev <sobolev.ax@gmail.com>
2019-04-07 15:18:23 +0300

    Added an example of reducer function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('Hello Redux!');
+
+const reducer = (state = 0, action) => {
+
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+
+    default:
+      return state;
+  }
+
+}
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'INC' });
+console.log('state: ', state);
+
+state = reducer(state, { type: 'INC' });
+console.log('state: ', state);
