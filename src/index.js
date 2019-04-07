9e90e0d0e626ac85374a393732dd17ff5094b7aa
Alexander Sobolev <sobolev.ax@gmail.com>
2019-04-07 17:03:09 +0300

    Added bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
 import {
+  createStore,
+  bindActionCreators,
+} from 'redux';
+
+import reducer from './reducer';
+import * as actions from './actions';
+
+const store = createStore(reducer);
+const { dispatch } = store;
+
+// const bindActionCreator = (action, dispatch) => (...args) => {
+//   return dispatch(action(...args));
+// }
+
+const {
   inc,
   dec,
   rnd,
-} from './actions';
-
-let store = createStore(reducer);
+} = bindActionCreators(actions, dispatch)
 
 const update = () => {
   console.log('state: ', store.getState());
@@ -20,20 +31,16 @@ store.subscribe(update);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  });
+  .addEventListener('click', dec);
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  });
+  .addEventListener('click', inc);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
 
-    store.dispatch(rnd(payload));
+    rnd(payload);
   });
